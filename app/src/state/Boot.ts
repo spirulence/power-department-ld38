f06754b159b7b5a56b4f58c8a76d569f47787a44
Cameron Seebach <pannewb@gmail>
2017-04-15 00:38:25 -0500

    code reformat, background color

--- app/src/state/Boot.ts
+++ app/src/state/Boot.ts
@@ -1,14 +1,14 @@
 export class Boot extends Phaser.State {
-  preload() {
-    this.load.image('preload-bar', 'assets/loading.png');
-  }
+    preload() {
+        this.load.image('preload-bar', 'assets/loading.png');
+    }
 
-  create() {
-    this.game.stage.backgroundColor = 0xFFFFFF;
+    create() {
+        this.game.stage.backgroundColor = 0x000000;
 
-    this.input.maxPointers = 1;
-    this.stage.disableVisibilityChange = true;
+        this.input.maxPointers = 1;
+        this.stage.disableVisibilityChange = true;
 
-    this.game.state.start('preload');
-  }
+        this.game.state.start('preload');
+    }
 }
--- app/src/state/States.ts
+++ app/src/state/States.ts
@@ -1,5 +1,4 @@
-
-import { Main } from './Main'
-import { Preload } from './Preload';
-import { Boot } from './Boot';
-export { Boot, Preload, Main }
+import {Main} from './Main'
+import {Preload} from './Preload';
+import {Boot} from './Boot';
+export {Boot, Preload, Main}
