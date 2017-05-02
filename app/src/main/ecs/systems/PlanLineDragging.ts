1535119c50b17e20d50aa894ff2fe9fc1aac1f7d
spirulence <pannewb@gmail.com>
2017-05-02 05:44:26 -0500

    partly working drag lines

--- app/src/main/ecs/systems/PlanLineDragging.ts
+++ app/src/main/ecs/systems/PlanLineDragging.ts
@@ -11,29 +11,21 @@ export class PlanLineDragging implements System{
     }
 
     process(entities: EntityManager): void {
-        // let targets = entities.queryComponents([Planned, TilePosition]).concat(entities.queryComponents([Built, TilePosition]));
 
         let dragging = entities.queryComponents([Dragging]).slice();
 
         for(let dragged of dragging){
-            if(dragged.dragging.ended){
-                entities.removeEntity(dragged);
-                continue;
-            }
-
             let current = dragged.dragging.current.clone();
             let x = Math.floor(current.x / 8);
             let y = Math.floor(current.y / 8);
             let source = {x:Math.floor(dragged.dragging.start.x / 8), y:Math.floor(dragged.dragging.start.y / 8)};
             let destination = {x:x, y:y};
-            this.planLine.addLine(source, destination, true);
-            // for(let target of targets){
-            //     if(target.tilePosition.x == x && target.tilePosition.y == y){
-            //         matched.push(dragged);
-            //         let source = {x:Math.floor(dragged.dragging.start.x / 8), y:Math.floor(dragged.dragging.start.y / 8)};
-            //         this.planLine.addLine(source, target.tilePosition, true);
-            //     }
-            // }
+            if(dragged.dragging.ended){
+                entities.removeEntity(dragged);
+                this.planLine.addLine(source, destination, false);
+            }else{
+                this.planLine.addLine(source, destination, true);
+            }
         }
     }
 
